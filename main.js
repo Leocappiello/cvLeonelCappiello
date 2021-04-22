61e9fccb67b432bc8747c1ba683486caed8ca497
Leo Cappiello <leonelcappiello@gmail.com>
2021-04-22 18:15:22 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('works')
+
+
+//key value 
+let keys = document.getElementsByClassName('key')
+let values = document.getElementsByClassName('valueText')
+
+for(i = 0; i<  keys.length; i++){
+    keys[i].innerHTML = `<q>${keys[i].innerHTML}</q>:`;
+}
+for(i = 0; i<  values.length-1; i++){
+    values[i].innerHTML = `<q>${values[i].innerHTML}</q>,`;
+}
